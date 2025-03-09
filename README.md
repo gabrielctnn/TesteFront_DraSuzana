80732413325b1b6da78871cef494c6aa1f209a06
Gabriel <gabriel.henrique.cm.2007@gmail.com>
2025-03-09 14:36:03 -0300

    up do site

---
+++ README.md
@@ -0,0 +1,3 @@
+[Teste - Frontend] Gabriel Henrique Caetano de Melo
+email : gabriel.henrique.cm.2007@gmail.com
+contato : +55 11 94394-9247
---
+++ js/index.js
@@ -0,0 +1,29 @@
+// botao consulta nav
+function scrollParaBaixo() {
+    document.getElementById("consulta").scrollIntoView({ behavior: "smooth" });
+}
+
+// interação na pagina de perguntas e respostas
+function toggleResposta(elemento) {
+    let resposta = elemento.nextElementSibling;
+    let estaVisivel = resposta.style.display === "block";
+
+    resposta.style.display = estaVisivel ? "none" : "block";
+    elemento.classList.toggle("verde", !estaVisivel);
+
+
+    elemento.classList.toggle("verde", !estaVisivel);
+    elemento.classList.toggle("ativa", !estaVisivel);
+}
+
+// slide home 
+
+const imagens = ["/assets/index/img1.png", "/assets/index/img2.png"];
+let index = 0;
+
+function trocarImagem() {
+    index = (index + 1) % imagens.length; 
+    document.getElementById("slide").src = imagens[index];
+}
+
+setInterval(trocarImagem, 5000);
